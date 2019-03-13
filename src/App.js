a6d8c9b0427679b2e3aff2b913f440dff46b6546
Raphael Bridi <raphael.bridi@bnc.ca>
2019-03-13 10:51:16 -0400

    added the tab

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
 import "react-bulma-components/dist/react-bulma-components.min.css";
-import { Button, Form, Dropdown } from "react-bulma-components";
 import "./App.scss";
 import SerialConnect from "./components/SerialConnect/SerialConnect";
 import TabConfig from "./components/TabConfig/TabConfig";
 
 class App extends Component {
-  state = {
-    gender: ""
-  };
-
-  onChange = evt => {
-    const value = evt.target.value;
-    this.setState({
-      [evt.target.name]: value
-    });
-  };
-
   render() {
     return (
       <div className="App">
@@ -32,7 +20,7 @@ class App extends Component {
           </div>
           <div className="Configuration">
             <div className="Tab">
-              <TabConfig />
+              <TabConfig onActiveTab={tab => console.log(tab)} />
             </div>
           </div>
         </div>
--- src/components/TabConfig/TabConfig.js
+++ src/components/TabConfig/TabConfig.js
@@ -1,18 +1,70 @@
-import React, { useState } from "react";
-import { Tabs } from "react-bulma-components";
+import React, { useState, useEffect } from "react";
+import { Tabs, Form, Button } from "react-bulma-components";
 import "./TabConfig.scss";
 
 const TabConfig = props => {
+  const tabs = {
+    MIN: "Min",
+    MAX: "Max",
+    POS1: "Pos1",
+    POS2: "Pos2",
+    POS3: "Pos3"
+  };
+
+  const [activeTab, setActiveTab] = useState(tabs.MIN);
+  const [text, setText] = useState("");
+
+  //watch for activeTab change
+  useEffect(() => {
+    props.onActiveTab(activeTab);
+  }, [activeTab]);
+
+  const updateActiveTab = event => {
+    const selected = event.target.text;
+    if (selected === undefined) return;
+    setActiveTab(selected);
+  };
+
   return (
     <div className="TabConfig">
-      <Tabs type={"boxed"}>
-        <Tabs.Tab active>Min</Tabs.Tab>
-        <Tabs.Tab>Max</Tabs.Tab>
-        <Tabs.Tab>Pos1</Tabs.Tab>
-        <Tabs.Tab>Pos2</Tabs.Tab>
-        <Tabs.Tab>Pos3</Tabs.Tab>
+      <Tabs type={"boxed"} onClick={event => updateActiveTab(event)}>
+        <Tabs.Tab active={activeTab === tabs.MIN}>{tabs.MIN}</Tabs.Tab>
+        <Tabs.Tab active={activeTab === tabs.MAX}>{tabs.MAX}</Tabs.Tab>
+        <Tabs.Tab active={activeTab === tabs.POS1}>{tabs.POS1}</Tabs.Tab>
+        <Tabs.Tab active={activeTab === tabs.POS2}>{tabs.POS2}</Tabs.Tab>
+        <Tabs.Tab active={activeTab === tabs.POS3}>{tabs.POS3}</Tabs.Tab>
       </Tabs>
-      <div className="TabContainer" />
+      <div className="TabContainer">
+        <Form.Textarea
+          onChange={event => setText(event.target.value)}
+          value={text}
+          className="TabContainer-textarea"
+          placeholder={activeTab + " configuration..."}
+        />
+
+        <div className="TabButtons">
+          <Button className="is-small" onClick={props.onTest} text>
+            test
+          </Button>
+          <Button
+            className="is-small"
+            onClick={props.onGetPosition}
+            color={"info"}
+          >
+            {`Get ${activeTab} From Arduino`}
+          </Button>
+        </div>
+      </div>
+      <div className="TabButtons">
+        <Button
+          onClick={props.onDisableServos}
+          color={"warning"}
+          className="is-outlined"
+        >
+          Disable servos
+        </Button>
+        <Button onClick={props.onConfig}>Send Configs To Arduino</Button>
+      </div>
     </div>
   );
 };
